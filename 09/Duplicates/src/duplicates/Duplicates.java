13a3534bb57e0783cfe5b938f3cb12dd6b51036f
LukeMcNemee <nemecl@seznam.cz>
2015-02-16 22:56:52 +0100

    Iterator, for each

--- 09/Duplicates/src/duplicates/Duplicates.java
+++ 09/Duplicates/src/duplicates/Duplicates.java
@@ -6,8 +6,10 @@
 
 package duplicates;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -16,7 +18,6 @@ import java.util.Random;
  * @author LukeMcNemee
  */
 public class Duplicates {
-
     /**
      * @param args the command line arguments
      */
@@ -24,18 +25,34 @@ public class Duplicates {
         List<Integer> list = new ArrayList<>();
         
         for(int i = 0; i < 100; i++){
-            list.add(new Integer(String.valueOf((int) (Math.random() * 50))));
+            list.add(new Integer(String.valueOf(
+                    (int) (Math.random() * 50 +1))));
         }
-        list = removeDuplicates(list);
         
-        Collections.sort(list);
+        for(Integer item : list){
+            System.out.print(item + ", ");
+        }
+        
+        System.out.println(" ");
+        Iterator iter = list.iterator();
+        while(iter.hasNext()){
+            System.out.print(iter.next() + ", ");
+        }
+        System.out.println(" ");
+        
+        System.out.println(multiplyList(list));
+        list = findNonDuplicates(list);
         System.out.println(list);
-        System.out.println(list.size());
+        System.out.println(sumList(list));
+        System.out.println(multiplyList(list));
+        
+        
         
     }
     
     public static List<Integer> removeDuplicates(List<Integer> list){
         List<Integer> newList = new ArrayList<>();
+        
         for(int i = 0; i < list.size(); i++){
             Integer o = list.get(i);
             if(!newList.contains(o)){
@@ -45,4 +62,42 @@ public class Duplicates {
         return newList;
     }
     
+    public static List<Integer> findNonDuplicates(List<Integer> list){
+        List<Integer> unique = new ArrayList<>();
+        List<Integer> duplicated = new ArrayList<>();
+        
+        for(Integer item : list){
+            if(!(duplicated.contains(item))){
+                if(!(unique.contains(item))){
+                    unique.add(item);
+                } else {
+                    unique.remove(item);
+                    duplicated.add(item);
+                }
+            }            
+        }
+        return unique;
+    }
+    
+    public static int sumList(List<Integer> list){
+        int sum = 0;
+        Iterator<Integer> iter = list.iterator();
+        
+        while(iter.hasNext()){
+            sum += iter.next();
+        }
+        
+        return sum;
+    }
+    
+    public static BigDecimal multiplyList(List<Integer> list){
+        BigDecimal multiple = new BigDecimal(1);
+        Iterator<Integer> iter = list.iterator();
+        
+        while(iter.hasNext()){
+            multiple = multiple.multiply(new BigDecimal(iter.next()));
+        }
+        
+        return multiple;
+    }
 }
